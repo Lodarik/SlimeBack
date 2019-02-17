9dc2855e809a484392d0948a0226ac2bba457f82
DD <dmitry@kishkin.su>
2019-02-17 18:02:36 +0300

    adding SQL base adapter

--- src/main/java/itmonopoly/ru/slime/Controller/AndroidController.java
+++ src/main/java/itmonopoly/ru/slime/Controller/AndroidController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import itmonopoly.ru.slime.DB.SQLconnect;
+
 @RestController
 @RequestMapping("Android")
-public class AndroidController{
-@GetMapping
-public String getWelcomeMessage() {
-	String welcomeMessage;
-	return welcomeMessage = "Hello!";
-}
+public class AndroidController {
+	SQLconnect db = new SQLconnect();
+
+	@GetMapping
+	public String getWelcomeMessage() {
+
+		String welcomeMessage = "Hello!";
+		return welcomeMessage;
+	}
 }
